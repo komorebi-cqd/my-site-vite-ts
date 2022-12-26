bd6b398bdf6625bade718813f7abf660221c0c7a
chenqiaoda <komorebi_cqd@163.com>
2022-12-26 23:41:00 +0800

    init

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ThemeProvider, ThemeConsumer } from 'styled-components'
+import GlobalStyle from './style'
 import App from './App'
-import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={{
+    fontFamily: '微软雅黑',
+    main: 'mediumseagreen'
+  }}>
+    <React.StrictMode>
+      <GlobalStyle />
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
+  ,
 )
